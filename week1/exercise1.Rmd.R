409b647983e9f1dfac4300468588a698e4b53fef
igarci20jh <igarci20@jh.edu>
2025-09-17 17:39:41 -0400

    changing scale

--- week1/exercise1.Rmd.R
+++ week1/exercise1.Rmd.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+setwd("/Users/cmdb/qb25-answers/week1")
 header <- c("chr", "start", "end", "count")
 df_kc <-read_tsv("/Users/cmdb/qb25-answers/week1/hg19-kc-count1.bed", col_names = header)
 
@@ -7,6 +7,6 @@ ggplot(data = df_kc,
        mapping = aes(x = start,
                      y = count)) +
   geom_line() +
-  facet_wrap("chr")
+  facet_wrap("chr", scales = "free")
 
 ggsave("exercise1.png")
